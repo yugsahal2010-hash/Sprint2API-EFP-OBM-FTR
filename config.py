531f4a6bb52f9ed0e90e7dcf61f1229e87cf090a
yugsahal2010-hash <yug.sahal2010@gmail.com>
2026-04-26 12:55:27 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+FANTASY_URL = "https://fantasypointshelperapi.onrender.com/weighted-average"
+BAYESIAN_URL = "https://opponentbayesian-helper.onrender.com/credibility"
+TREND_URL = "https://formtrend-helper.onrender.com/weights"
